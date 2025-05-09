877a6ba7801aeda21989939d97281815e645c980
Danny00117 <charly.protocol@gmail.com>
2025-05-09 03:05:27 -0600

    Update urls.py

--- app_base/urls.py
+++ app_base/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views.envio_api import listar_envios, crear_envio, actualizar_envio, eliminar_envio
 
 urlpatterns = [
-    path('api/envios/', listar_envios),
-    path('api/envios/crear/', crear_envio),
-    path('api/envios/<uuid:envio_id>/actualizar/', actualizar_envio),
-    path('api/envios/<uuid:envio_id>/eliminar/', eliminar_envio),
+    path('envios/', listar_envios),
+    path('envios/crear/', crear_envio),
+    path('envios/<uuid:envio_id>/actualizar/', actualizar_envio),
+    path('envios/<uuid:envio_id>/eliminar/', eliminar_envio),
 ]
