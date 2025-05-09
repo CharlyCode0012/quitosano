733fa134f1947347c04d245ff9203c3c54304e49
Danny00117 <charly.protocol@gmail.com>
2025-05-09 03:57:47 -0600

    Update supabase_client.py

--- app_base/supabase_client.py
+++ app_base/supabase_client.py
@@ -1,20 +1,8 @@
+# app_base/supabase_client.py
 from supabase import create_client
 from decouple import config
-import sys
 
-try:
-    SUPABASE_URL = config("SUPABASE_URL")
-    SUPABASE_KEY = config("SUPABASE_KEY")
-except Exception as e:
-    print("❌ Error cargando variables:", e)
-    sys.exit("Variables de entorno faltantes")
-
-if not SUPABASE_URL or not SUPABASE_KEY:
-    print("❌ Variables vacías:")
-    print("SUPABASE_URL =", SUPABASE_URL)
-    print("SUPABASE_KEY =", SUPABASE_KEY)
-    sys.exit("❌ No se encontraron las variables necesarias")
-
-print("✅ SUPABASE_URL leída correctamente:", SUPABASE_URL)
+SUPABASE_URL = config("SUPABASE_URL")
+SUPABASE_KEY = config("SUPABASE_KEY")
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
