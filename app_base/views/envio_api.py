541526c1f25b384956ebf6321a51b0015be75785
Danny00117 <charly.protocol@gmail.com>
2025-05-09 12:33:31 -0600

    Fix test

--- app_base/views/envio_api.py
+++ app_base/views/envio_api.py
@@ -26,7 +26,7 @@ def parse_envio(data: dict) -> Envio:
 
 @require_http_methods(["GET"])
 def listar_envios(request):
-    response = supabase.table("envio").select("*").execute()
+    response = supabase.table("envios").select("*").execute()
     return JsonResponse(response.data, safe=False)
 
 
