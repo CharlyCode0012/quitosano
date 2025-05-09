3a976668d6aa2406f8aad8b6bd32b717ea1dbebb
Danny00117 <charly.protocol@gmail.com>
2025-05-09 02:30:14 -0600

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -125,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOSTS', default='localhost').split(',')
