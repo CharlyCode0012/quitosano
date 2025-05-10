04b19ebd6ab0c216ecac3da83b226a32aab98bfb
AsuraBeliever <alansethmanjarrez@gmail.com>
2025-05-09 19:39:34 -0600

    add function in Transporte, id_transporte now gets the next id correctly

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 from supabase import create_client, Client
 from pydantic.dataclasses import dataclass
-
+from datetime import datetime
 
 @dataclass
 class Supabase:
@@ -25,6 +25,9 @@ class BaseTable:
     
     def list_all(self):
         return self.table.select("*").execute()
+    
+    def get_last_record(self, field: str, field_2: str):
+        return self.table.select(field).order(field_2, desc=True).limit(1).execute()
 
 class Users(BaseTable):
     def __init__(self, db : Supabase):
@@ -37,13 +40,22 @@ class Transporte(BaseTable):
     def __init__(self, db : Supabase):
         super().__init__(db, "transporte_quitosano")
 
-    def add(self, id_trasnporte: str, origen: str, destino: str, producto: str, cantidad_kg: int):
+    def add(self, origen: str, destino: str, producto: str, cantidad_kg: int):
+        current_year = str(datetime.now().year)
+        current_date = str(datetime.now())
+        last_id = self.get_last_record("id_transporte", "Fecha")
+        last_id = last_id.data[0]["id_transporte"]
+        last_id = str(last_id).split("-")[-1]
+        last_id = int(last_id) + 1
+        next_id = str(last_id).zfill(3)
+        id_transporte = "TR-"+current_year+"-"+next_id
         self.insert({
-            "id_transporte" : id_trasnporte,
+            "id_transporte" : id_transporte,
             "origen" : origen, 
             "destino" : destino,
             "producto" : producto,
-            "cantidad_kg" : cantidad_kg
+            "cantidad_kg" : cantidad_kg,
+            "Fecha" : current_date
         })
 
 #need sensor to gather data
